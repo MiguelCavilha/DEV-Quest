11a27ac5b87e2ece91f685cb78065d2abe4795ea
Luiz Miguel Cavilha <miguelcavilha@outlook.com>
2023-07-05 19:40:35 -0300

    aulas atualizadas

---
+++ modulo-react-base/05-create-react-app/primeiro-app/src/components/card/card.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './card.css'
+
+const Card = ({ children, color, showCardColor }) => {
+    return (
+        <div className="card" 
+        style={{ backgroundColor: color }} 
+        onClick={() => showCardColor(color)}>
+            {children}
+        </div>
+
+    )
+}
+
+Card.defaultProps = {
+    color: 'orange'
+}
+// class Card extends React.Component {
+//     render() {
+//         return(
+//             <div className="card">
+//                 <h3>{this.props.title}</h3>
+//                 <p>Esse é um texto do card</p>
+//             </div>
+//         )
+//     }
+
+// }
+
+export default Card
---
+++ modulo-react-base/projeto-exemplo/index.js
@@ -0,0 +1,4 @@
+const root = document.getElementById ('root')
+const p = React.createElement('p', null, 'olá, eu sou um paragrafo')
+
+ReactDOM.render(p, root)
